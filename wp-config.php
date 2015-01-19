1c909b6547d246b67bd61b2675b8697cc2a8ab02
destyjustc <kjoshy12@gmail.com>
2015-01-18 22:01:37 -0800

    turn on debug

--- wp-config.php
+++ wp-config.php
@@ -82,7 +82,7 @@ define('WPLANG', 'en_CA');
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
  * in their development environments.
  */
-define('WP_DEBUG', false);
+define('WP_DEBUG', true);
 
 /* That's all, stop editing! Happy blogging. */
 
